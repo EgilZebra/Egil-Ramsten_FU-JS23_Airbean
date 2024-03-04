89768654d4029d47ecce833223f9257226e2653c
Egil Ramsten <egil.ramsten@hotmail.com>
2024-03-04 21:46:33 +0100

    Updated the scss mixins-file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { 
+  Route, 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider 
+} from "react-router-dom"
+import ErrorPage from "./pages/errorpage/ErrorPage"
+import LandingPage from "./pages/landingpage/LandingPage"
+import AboutPage from "./pages/aboutpage/AboutPage"
+import MenyPage from "./pages/menypage/MenyPage"
+import ProfilePage from "./pages/profilepage/ProfilePage"
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route errorElement={<ErrorPage />}>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/about" element={<AboutPage/>} />
+        <Route path="/meny" element={<MenyPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+      </Route>
+  )
+)
 
 function App() {
 
-
   return (
     <>
+      <RouterProvider router={router} />
     </>
   )
 }
